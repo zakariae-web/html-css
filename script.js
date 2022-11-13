84578784c57122e440e3420b065ecfcef7b7550d
zakariae-web <zakariaebelouadif@gmail.com>
2022-11-13 13:47:08 +0100

    portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".auto-name",{
+    strings:["web developper","student"],
+    typeSpeed: 50,
+    backSpeed: 20,
+    smartBackspace: true,
+
+    backDelay: 2000,
+    loop: true
+})
